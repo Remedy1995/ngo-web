8707a8aa78a908b52606990d60ba36aeeac2946b
Remedy1995 <adjetadjetey45@gmail.com>
2024-08-24 00:36:21 +0000

    created the home page

--- ngosite/views.py
+++ ngosite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def index(self): 
+     template = loader.get_template('index.html')
+     return HttpResponse(template.render())
